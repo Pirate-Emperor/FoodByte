5df4347e2b173ee83ceeaa78114b7d4f96600c9d
Pirate-Emperor <piratekingrahul@gmail.com>
2023-08-15 15:46:00 +0100

    Add Appetizers\FoodByteApp.swift

---
+++ Appetizers/FoodByteApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodByteApp.swift
+//  FoodByte
+//
+//  Created by Pirate-Emperor on 14/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct FoodByteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FoodByteTabView()
+        }
+    }
+}
