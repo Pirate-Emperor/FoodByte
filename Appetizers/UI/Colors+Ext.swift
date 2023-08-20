678520703a74aa2af4069076fec09f58f3528ce5
Pirate-Emperor <piratekingrahul@gmail.com>
2023-08-20 14:11:00 +0100

    Add Appetizers\UI\Colors+Ext.swift

---
+++ Appetizers/UI/Colors+Ext.swift
@@ -0,0 +1,13 @@
+//
+//  Colors+Ext.swift
+//  FoodByte
+//
+//  Created by Pirate-Emperor on 14/08/2023
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let brandPrimaryy = Color("brandPrimary")
+}
