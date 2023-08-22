065151676e7f63cb52a2f4acce1efd3de1c51848
Pirate-Emperor <piratekingrahul@gmail.com>
2023-08-22 04:01:00 +0100

    Add Appetizers\UI\Screen\OrderView.swift

---
+++ Appetizers/UI/Screen/OrderView.swift
@@ -0,0 +1,21 @@
+//
+//  OrderView.swift
+//  FoodByte
+//
+//  Created by Pirate-Emperor on 14/08/2023
+//
+
+import SwiftUI
+
+struct OrderView: View {
+    var body: some View {
+        NavigationView {
+            Text("Orders")
+                .navigationTitle("Orders")
+        }
+    }
+}
+
+#Preview {
+    OrderView()
+}
